4c79dee51e7e1dc4ce73b49749e9184927383937
Connor Lagana <connor.lagana@gmail.com>
2020-04-23 02:18:20 -0400

    idc

--- FBSW/ContentView.swift
+++ FBSW/ContentView.swift
@@ -8,12 +8,27 @@
 
 import SwiftUI
 
+
+struct Post: Identifiable {
+    let id: Int
+    let username, text, imageName, userImageName: String
+}
+
+
 struct ContentView: View {
     
-    let posts = ["1", "2", "3"]
+    let posts: [Post] = [
+        .init(id: 0, username: "Alexandra Daddario", text: "Just had the time of my life at Arches National Park™. I wish Zac Efron was here so we can workout together and get super swoll!", imageName: "arches", userImageName: "alex"),
+        .init(id: 1, username: "Zac Efron", text: "In hawaii for the weekend. it's soooo much fun. Wish alex was here!!!!", imageName: "hawaii", userImageName: "zac")]
+    
+    init() {
+        UITableView.appearance().separatorColor = .clear
+    }
     
     var body: some View {
+        
         NavigationView {
+            
             List {
                 
                 ScrollView {
@@ -27,10 +42,10 @@ struct ContentView: View {
                             Text("Group1")
                         }
                     }
-                }.frame(height: 100)
+                }.frame(height: 100).background(Color.red)
                 
-                ForEach(posts, id: \.self) { post in
-                    PostView()
+                ForEach(posts, id: \.id) { post in
+                    PostView(post: post)
                 }
                 
             }.navigationBarTitle(Text("Groups"))
@@ -40,32 +55,37 @@ struct ContentView: View {
 }
 
 struct PostView: View {
+    
+    let post: Post
+    
     var body: some View {
-        VStack (alignment: .leading) {
+        VStack (alignment: .leading, spacing: 12) {
             HStack {
-                Image("alex")
+                Image("\(post.userImageName)")
                     .resizable()
+                    //I don't know how to fix the stretch, this works but it makes the image smaller for some odd reason (not fixed amount tho)
+//                    .scaledToFit()
                     .clipShape(Circle())
                     .frame(width: 60, height: 60)
                 VStack (alignment: .leading, spacing: 4) {
-                    Text("Alexandra Daddario").font(.headline)
+                    Text("\(post.username)").font(.headline)
                     Text("Posted 18 minutes ago").font(.subheadline)
                 }.padding(.leading, 8)
                 
             }.padding(.leading, 16).padding(.top, 16)
             
-            Text("Just had the time of my life at Arches National Park™. I wish Zac Efron was here so we can workout together and get super swoll!")
+            Text("\(post.text)")
                 .lineLimit(nil)
                 .padding(.leading, 16)
                 .padding(.trailing, 16)
                 
-            Image("arches")
+            Image("\(post.imageName)")
                 
                 .resizable()
                 .scaledToFit()
                 .frame(width:  400, height: 260)
                 
-        }.padding(.leading, -20)
+            }.padding(.leading, -20)
     }
 }
 
